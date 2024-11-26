546c8a36f0257e197f8cc43a56f1835d9df61eed
Shruti Sharada S <67855644+Shrutisharada99@users.noreply.github.com>
2024-11-26 05:55:17 +0530

    Update data_ingestion.py
    Classes revision

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,8 +8,7 @@ from dataclasses import dataclass
 @dataclass
 class DataIngestionConfig:
     raw_data_path:str=os.path.join("artifacts","data.csv")
-    train_data_path:str=os.path.join("artifacts","train.csv")
-    test_data_path:str=os.path.join("artifacts","test.csv")
+    # test-train split
 
 class DataIngestion:
     def __init__(self):
